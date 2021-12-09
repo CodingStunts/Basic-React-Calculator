2750935f5b52fef048406e6cf86e959fbae1f27d
CodingStunts <jayhayes13@hotmail.co.uk>
2021-12-09 15:27:17 +0000

    imporoved functionality

--- project/src/App.js
+++ project/src/App.js
@@ -1,69 +1,71 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const addNumInput = () => {
-    let defaultFigure = 0;
-    const buttons = [
-      "AC",
-      "+",
-      "-",
-      "/",
-      "*",
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      0,
-      ".",
-      "=",
-    ];
-    let [number, setNum] = useState(defaultFigure);
-    concatInput = () => {
-      setNum((number) => {
-        return String(defaultFigure) + String(number);
-      });
-    };
+  let [display, setDisplay] = useState(0);
+
+  const concatInput = (digit) => {
+    setDisplay((oldDisplay) => {
+      console.log(oldDisplay);
+      /*       if (oldDisplay === "0") {
+        console.log("hello");
+        return oldDisplay.unshift([0]);
+      } else { */
+      return String(oldDisplay) + String(digit);
+      //}
+    });
+  };
+
+  const numberButtons = [
+    { 0: 0 },
+    { 1: 1 },
+    { 2: 2 },
+    { 3: 3 },
+    { 4: 4 },
+    { 5: 5 },
+    { 6: 6 },
+    { 7: 7 },
+    { 8: 8 },
+    { 9: 9 },
+  ];
+
+  let operationButtons = [
+    { 0: "-" },
+    { 1: "+" },
+    { 2: "/" },
+    { 3: "*" },
+    { 4: "." },
+    { 5: "AC" },
+    { 6: "=" },
+  ];
+
+  const MakeNumberButtons = () => {
+    return (
+      <div>
+        <h1>Buttons</h1>
+        {numberButtons.map((button, i) => (
+          <button onClick={() => concatInput(button[i])}>{button[i]}</button>
+        ))}
+      </div>
+    );
+  };
+
+  const MakeOperationButtons = () => {
+    return (
+      <div>
+        {operationButtons.map((button, i) => (
+          <button onClick={() => concatInput(button[i])}>{button[i]}</button>
+        ))}
+      </div>
+    );
   };
 
   return (
     <div className="App">
       <h1>Calculator</h1>
-      <label>{defaultFigure}</label>
-      {buttons.map((button) => (
-        <button onClick={() => concatInput(button)}>{button}</button>
-      ))}
-
-      {/*  <label>0</label>
-      <br></br>
-      <button>AC</button>
-      <button id="+">+</button>
-      <button id="-">-</button>
-      <br></br>
-      <button id="/">/</button>
-      <button id="*">*</button>
-      <button onClick={() => setNum(1)}>1)</button>
-      <button id="2">2</button>
-      <button id="3">3</button>
-      <br></br>
-      <button id="4">4</button>
-      <button id="5">5</button>
-      <button id="6">6</button>
-      <br></br>
-      <button id="7">7</button>
-      <button id="8">8</button>
-      <button id="9">9</button>
-      <br></br>
-      <button id="0">0</button>
-      <button id=".">.</button>
-      <button id="=">=</button> */}
-      <addNumInput />
+      <label>{display}</label>
+      <MakeNumberButtons />
+      <MakeOperationButtons />
     </div>
   );
 }
